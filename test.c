1936871aac682ee2165756d789495f358dd310a8
Big Brother <63774297+zhb781619517@users.noreply.github.com>
2020-10-17 22:57:50 +0800

    再次修改
    还是修改，未完成

--- test.c
+++ test.c
@@ -24,10 +24,10 @@ typedef struct {
     int num;
 }pkNode, * pk;          //停车场的栈类型
 
-void QPush(QueueList, Car);
-Car QPop(QueueList);
-void SPush(pk, Car);
-Car SPop(pk);
+void QPush(QueueList, Car*);
+Car *QPop(QueueList);
+void SPush(pk, Car*);
+Car *SPop(pk);
 
 int main()
 {
@@ -36,7 +36,7 @@ int main()
     QueuePtr a = (QueuePtr)malloc(sizeof(QNode));                   //建立空的链队列
     //Q->next = NULL;                                                 //初定向
     Q->front = Q->rear = a;                                   //赋予队列头和尾
-    Car b, c;                                                          //输入中介
+    Car *b, *c;                                                          //输入中介
     char d;
     packing1 = (pk)malloc(sizeof(pkNode));
     packing2 = (pk)malloc(sizeof(pkNode));                          //申请停车场内存
@@ -46,7 +46,7 @@ int main()
     {
         j=0;
         printf("请输入：\n");
-        scanf("%c %d %d", &d, &b.num, &b.time);
+        scanf("%c %d %d", &d, &b->num, &b->time);
         if (d == 'E')
         {
             printf("结束\n");
@@ -57,29 +57,30 @@ int main()
             if (packing1->num > N - 1)
             {
                 QPush(Q, b);
-                printf("车牌%d，时间%d，便道位置%d\n",b.num,b.time,Q->length);
+                printf("车牌%d，时间%d，便道位置%d\n",b->num,b->time,Q->length);
             }
             else
             {
                 SPush(packing1, b);
-                printf("车牌%d，时间%d，停车场位置%d\n",b.num,b.time,packing1->num);
+                printf("车牌%d，时间%d，停车场位置%d\n",b->num,b->time,packing1->num);
             }
         }
         else if (d == 'D')
         {
-            for(i=N-1;i>=0;i++)
+            for(i=packing1->num-1;i>=0;i++)
             {
-                if(packing1->data[i].num==b.num)
+                if(packing1->data[i].num==b->num)
                 {
-                    k=b.time-packing1->data[i].time;
-                    printf("开始时间%d，结束时间%d，经过的时间%d，收费%d\n",packing1->data[i].time,b.time,k,k*FEE);
-                    h=b.num;
-                    SPop(packing1);
+                    k=b->time-packing1->data[i].time;
+                    printf("开始时间%d，结束时间%d，经过的时间%d，收费%d\n",packing1->data[i].time,b->time,k,k*FEE);
+                    h=b->time;
+                    free(SPop(packing1));
                     break;
                 }
                 else
                 {
                     c=SPop(packing1);
+                    printf("%d	%d\n",c->num,c->time);
                     SPush(packing2,c);
                     j++;
                 }
@@ -93,9 +94,9 @@ int main()
             if(Q->length)
             {
                 c=QPop(Q);
-                c.time=h;
+                c->time=h;
                 SPush(packing1,c);
-                printf("车牌号为%d的车辆从便道进入停车场%d处，进入停车场的时间为%d\n",c.num,packing1->num,h);
+                printf("车牌号为%d的车辆从便道进入停车场%d处，进入停车场的时间为%d\n",c->num,packing1->num,h);
             }
         }
         while(getchar()!='\n'){}
@@ -103,38 +104,38 @@ int main()
     return 0;
 }
 
-void QPush(QueueList a, Car b)          //尾部入，头部出
+void QPush(QueueList a, Car *b)          //尾部入，头部出
 {
     QueuePtr temp=malloc(sizeof(QNode));
-    temp->data.num=b.num;
-    temp->data.time=b.time;
+    temp->data.num=b->num;
+    temp->data.time=b->time;
     temp->next=a->rear->next;
     a->rear->next=temp;
     a->rear=temp;
     a->length++;
 }
 
-Car QPop(QueueList a)
+Car *QPop(QueueList a)
 {
-    Car temp;
-    temp.num=a->front->data.num;
-    temp.time=a->front->data.time;
+    Car *temp=malloc(sizeof(Car));
+    temp->num=a->front->data.num;
+    temp->time=a->front->data.time;
     a->length--;
     return temp;
 }
 
-void SPush(pk a, Car b)
+void SPush(pk a, Car *b)
 {
-    a->data[a->num].num=b.num;
-    a->data[a->num].time=b.time;
+    a->data[a->num].num=b->num;
+    a->data[a->num].time=b->time;
     a->num++;
 }
 
-Car SPop(pk a)
+Car *SPop(pk a)
 {
-    Car temp;
-    temp.num=a->data[a->num].num;
-    temp.time=a->data[a->num].time;
+    Car *temp=malloc(sizeof(Car));
+    temp->num=a->data[a->num].num;
+    temp->time=a->data[a->num].time;
     a->num--;
     return temp;
 }
